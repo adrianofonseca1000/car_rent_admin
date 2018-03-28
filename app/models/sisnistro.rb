3ca3d862e97de713e6d9182b9872ca7772921187
Adriano <adriano.fonseca1000@gmail.com>
2018-03-28 22:51:40 +0000

    Criando Projeto car_rental

---
+++ app/models/sisnistro.rb
@@ -0,0 +1,2 @@
+class Sisnistro < ApplicationRecord
+end
---
+++ test/models/sisnistro_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SisnistroTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
